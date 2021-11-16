4f622c56dfcb4c381ec4815194328e221b220dad
Mihaela Dumbrava <mdumbrava@evertz.com>
2021-11-16 13:30:19 -0500

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+ import React from 'react'
+ 
+ const Navbar = () => {
+     return (
+         <div>
+          <h1 style={{color: 'purple'}}>Navbar</h1>   
+         </div>
+     )
+ }
+ 
+ export default Navbar;
+ 
